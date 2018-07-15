17b866e85ea290ece06c7110efc8528b6a480aa4
Cici <cici.harrison@gmail.com>
2018-07-15 16:53:29 -0700

    display an array in the console log of the gathered topping costs but does not add them to the total cost, but rather just adds the first checked topping cost. Need to fix this.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,43 @@
 //Business Logic
 //Set up Constructor
-function Pizza(size, toppings) {
+function Pizza(size, toppingCost) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppingCost = toppingCost;
 }
 
 //Set up Prototype
 Pizza.prototype.info = function() {
-  return this.size + "with" + this.toppings;
+  return "One " + this.size + " inch pie with " + this.toppingCost + " topping";
 }
+
+var pizzaTruth = [];
 
 //UI Logic
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
+    $("#pizza-size option:selected").each(function() {
+      var size = $(this).val();
+    });
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var toppingCost = $(this).val();
+      pizzaTruth.push(toppingCost);
+    });
 
     var size = $("#pizza-size option:selected").val();
-    console.log(size);
-    var toppings = parseInt($("input[name=toppings]:checked").val());
-    console.log(toppings);
-    var total = parseFloat(size) + parseFloat(toppings);
-    console.log(total);
-    var newPizza = new Pizza(size, toppings);
+    //console.log(size);
+    var toppingCost = parseInt($("input:checkbox[name=toppings]:checked").val());
+    //console.log(toppingCost);
+
+    var total = parseFloat(size) + parseFloat(pizzaTruth);
+    console.log(pizzaTruth);
+    var newPizza = new Pizza(size, toppingCost);
+    //console.log(newPizza);
+    //$("#result").empty();
+
 
     $("#result").text(newPizza.info());
-    console.log(newPizza);
     $("#result").text("$" + total + ".00");
-    $("#result").show(total);
-
-
+    $("#result").show();
   });
-});
-
-//var results = [];
-//var size = parseInt($("#pizza-size option:selected"));
-//var toppings = parseInt($("input:checkbox[name=toppings]:checked"));
+    });
